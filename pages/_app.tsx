a61ed16c2a6e06c45f63ea267dfce01046a4af54
gaurease <gaurisunilbharat@gmail.com>
2023-04-14 19:58:12 +0530

    changed network sec

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,14 @@ import type { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client/react'
 import client from '../client'
 import ProtectedRoute from '@/ProtectRoute'
+import Head from 'next/head'
 export default function App({ Component, pageProps,router }: AppProps) {
 
   return <ApolloProvider client={client}>
     <ProtectedRoute router={router}>
+      <Head>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
     <Component {...pageProps} />
     </ProtectedRoute>
   </ApolloProvider>
